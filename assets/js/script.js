b5eafedd31adde3508fdf7f984790bbc365cee19
KingCreatBug <trunghoang.240500@gmail.com>
2024-03-14 17:14:28 +0700

    commit Menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,3 +27,30 @@ function load(selector, path) {
             window.dispatchEvent(new Event("template-loaded"));
         });
 }
+
+// Chuyển tab
+window.addEventListener("template-loaded", () => {
+    const tabsSelector = "menu-tab__item";
+    const contentsSelector = "menu-tab__content";
+
+    const tabActive = `${tabsSelector}--active`;
+    const contentActive = `${contentsSelector}--active`;
+
+    const tabContainers = $$(".js-tabs");
+    tabContainers.forEach((tabContainer) => {
+        const tabs = tabContainer.querySelectorAll(`.${tabsSelector}`);
+        const contents = tabContainer.querySelectorAll(`.${contentsSelector}`);
+        tabs.forEach((tab, index) => {
+            tab.onclick = () => {
+                tabContainer
+                    .querySelector(`.${tabActive}`)
+                    ?.classList.remove(tabActive);
+                tabContainer
+                    .querySelector(`.${contentActive}`)
+                    ?.classList.remove(contentActive);
+                tab.classList.add(tabActive);
+                contents[index].classList.add(contentActive);
+            };
+        });
+    });
+});
