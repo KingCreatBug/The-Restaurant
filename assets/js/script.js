5cf5d9e3ff0039bf976d17d0fc855f3bc77bb31f
KingCreatBug <trunghoang.240500@gmail.com>
2024-03-18 01:03:55 +0700

    commit responsive 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,10 +28,37 @@ function load(selector, path) {
         });
 }
 
-// Chuyển tab
+// Chuyển tab menu
 window.addEventListener("template-loaded", () => {
     const tabsSelector = "menu-tab__item";
     const contentsSelector = "menu-tab__content";
+
+    const tabActive = `${tabsSelector}--active`;
+    const contentActive = `${contentsSelector}--active`;
+
+    const tabContainers = $$(".js-tabs");
+    tabContainers.forEach((tabContainer) => {
+        const tabs = tabContainer.querySelectorAll(`.${tabsSelector}`);
+        const contents = tabContainer.querySelectorAll(`.${contentsSelector}`);
+        tabs.forEach((tab, index) => {
+            tab.onclick = () => {
+                tabContainer
+                    .querySelector(`.${tabActive}`)
+                    ?.classList.remove(tabActive);
+                tabContainer
+                    .querySelector(`.${contentActive}`)
+                    ?.classList.remove(contentActive);
+                tab.classList.add(tabActive);
+                contents[index].classList.add(contentActive);
+            };
+        });
+    });
+});
+
+// Chuyển tab service
+window.addEventListener("template-loaded", () => {
+    const tabsSelector = "service-tab__item";
+    const contentsSelector = "service-tab__img";
 
     const tabActive = `${tabsSelector}--active`;
     const contentActive = `${contentsSelector}--active`;
